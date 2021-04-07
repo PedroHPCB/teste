55cca835004e80091efba0c1c0168c3a1238dc09
PedroHPCB <pedrohpcbrandao@gmail.com>
2021-04-07 11:44:58 -0300

    uai

--- exercicioDeHoje/js/pessoa.js
+++ exercicioDeHoje/js/pessoa.js
@@ -3,14 +3,18 @@ var nome = prompt('qual o seu nome ?');
 var idade = parseInt(prompt('qual a sua idade ?'));
 var sexo = prompt('qual seu sexo ?[M/F]:').toUpperCase().substr(0, 1);
 if (sexo == "M") {
-    sexo = "Masculino"
+    sexo = "Masculino";
 }
 
 else if (sexo == "F"){
-    sexo = "Feminino"
+    sexo = "Feminino";
 }
 else {
-    alert("escreva apenas feminino ou masculino.")
+    while (sexo != "M" || sexo != "F"){
+        alert("escreva apenas feminino ou masculino.");
+        var sexo = prompt('qual seu sexo ?[M/F]:').toUpperCase().substr(0, 1);
+    }
+    break
 }
 
 let pessoa = {
@@ -22,4 +26,3 @@ let pessoa = {
 document.write("<h2>Pessoa cadastrada<br></h2>", pessoa.nome,'<br>',
 pessoa.idade, '<br>', pessoa.sexo, '<br><br>')
 alterador();
-
